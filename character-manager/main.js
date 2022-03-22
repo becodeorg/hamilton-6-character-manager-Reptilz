f2bb0c8f7705467142e41b65b78bd81ecc485d13
Morlet <34801852+Reptilz@users.noreply.github.com>
2022-03-22 10:37:32 +0100

    amélioration des fonctions main.js

--- character-manager/main.js
+++ character-manager/main.js
@@ -2,8 +2,9 @@ import './style.scss'
 
 //list of characters
 async function getCharacter(){
+    const api_url = 'https://character-database.becode.xyz/characters';
     try {
-        const response = await axios.get('https://character-database.becode.xyz/characters');
+        const response = await axios.get(api_url);
         console.log(response);
     }
     catch (err) {
@@ -15,11 +16,16 @@ async function getCharacter(){
 
 //post character
 async function addCharacter(){
+    const api_url = 'https://character-database.becode.xyz/characters';
+    const name = "Itachi Uchiwa"
+    const description = "Itachi Uchiwa était un membre de l'Anbu issu du célèbre clan Uchiwa du village de Konoha. Il tua les membres de son clan dans ce qui sera plus tard connu comme le massacre du clan Uchiwa et déserta son village, devenant un ninja déserteur (Nukenin) de Rang S et un membre de l'Akatsuki qui devint partenaire avec Kisame Hoshigaki."
+    const shortDescription = "You should try an other character";
+
     try{
-        await axios.post('https://character-database.becode.xyz/characters', {
-            description: "Itachi Uchiwa était un membre de l'Anbu issu du célèbre clan Uchiwa du village de Konoha. Il tua les membres de son clan dans ce qui sera plus tard connu comme le massacre du clan Uchiwa et déserta son village, devenant un ninja déserteur (Nukenin) de Rang S et un membre de l'Akatsuki qui devint partenaire avec Kisame Hoshigaki.",
-            name: "Itachi Uchiwa",
-            shortDescription: "You should try an other character",
+        await axios.post(api_url, {
+            description: description,
+            name: name,
+            shortDescription: shortDescription,
         });
     }
     catch(err){
@@ -31,9 +37,10 @@ async function addCharacter(){
 //delete character
 async function deleteCharacter(){
     const id = "a4b57db1-a9ce-401b-af36-fcbbf712a86d"
+    const api_url = 'https://character-database.becode.xyz/characters/';
 
     try{
-        await axios.delete('https://character-database.becode.xyz/characters/' + id);
+        await axios.delete(api_url + id);
     }
     catch(err){
         console.log(err);
@@ -43,12 +50,16 @@ async function deleteCharacter(){
 //update character
 async function updateCharacter(){
     const id = "26c4917e-fba3-47de-85b6-475d4a2cf9b7";
+    const api_url = 'https://character-database.becode.xyz/characters/';
+    const name = "Itachi Uchiwa";
+    const description = "Hello update!";
+    const shortDescription = "this is a short description";
 
     try{
-        await axios.put('https://character-database.becode.xyz/characters/' + id, {
-            description: "Itachi Uchiwa était un membre de l'Anbu issu du célèbre clan Uchiwa du village de Konoha. Il tua les membres de son clan dans ce qui sera plus tard connu comme le massacre du clan Uchiwa et déserta son village, devenant un ninja déserteur (Nukenin) de Rang S et un membre de l'Akatsuki qui devint partenaire avec Kisame Hoshigaki.",
-            name: "Itachi Uchiwa",
-            shortDescription: "You should try an other character",
+        await axios.put(api_url + id, {
+            description: description,
+            name: name,
+            shortDescription: shortDescription,
         });
     }
     catch(err){
