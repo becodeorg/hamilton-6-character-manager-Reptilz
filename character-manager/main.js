9bbb3ef1bccaef17d3006c03fb2e2525e95e1bed
Morlet <34801852+Reptilz@users.noreply.github.com>
2022-03-23 11:45:03 +0100

    Listing des characters sur index.html

--- character-manager/main.js
+++ character-manager/main.js
@@ -3,28 +3,28 @@ import './style.scss'
 //list of characters
 async function getCharacter(){
     const api_url = 'https://character-database.becode.xyz/characters';
-    try {
-        const response = await axios.get(api_url);
-        const datas = response.data;
-        console.log(datas);
-        
-        const cards = document.getElementById('cards');
 
-        //New HTML Dynamic
+    //get des datas dans l'API
+    const response = await axios.get(api_url);
+    const datas = response.data;
+    console.log(datas);
+
+    const cards = document.getElementById('cards');
+
+    try {
+        //afficher les datas dans le HTML
         datas.forEach((data) => {
-           cards.innerHTML += 
-           `
-           <article id="card">
-                <div id="image">
-                    <img src="data:image/gif;base64,${data.image}" alt="image">
-                </div>
-                <h2>${data.name}</h2>
-                <p>${data.shortDescription}</p>
-                <input type="button" value="See character">
-           </article>
-           `;
-          
-        })
+            cards.innerHTML +=
+            `
+            <article class="card" id="card">
+                <img class="card__img" id="card-img" src="data:image/gif;base64,${data.image}" alt="img">
+                <h2 class="card__title" id="card-name">${data.name}</h2>
+                <p class="card__description" id="card-short-description">${data.shortDescription}</p>
+                <input class="card__btn" id="card-btn" type="button" value="See character">
+            </article>
+            `
+        });
+        
     }
     catch (err) {
         console.log(err);
